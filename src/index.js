b23e7b1bf3fa99ccf18f15b16e6037fc2720de02
felix <felix@webisoft.com>
2021-02-03 19:29:58 -0500

    import all new resolvers

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 const {PrismaClient} = require('@prisma/client')
-const {ApolloServer} = require('apollo-server')
+const {ApolloServer, PubSub} = require('apollo-server')
 const fs = require('fs')
 const path = require('path')
 const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
 const User = require('./resolvers/User')
 const Link = require('./resolvers/Link')
+const Vote = require('./resolvers/Vote')
+const Subscription = require('./resolvers/Subscription')
 const {getUserId} = require('./utils')
 
+const pubsub = new PubSub()
 const prisma = new PrismaClient()
 
-const resolvers = {Query, Mutation, User, Link}
+const resolvers = {Query, Mutation, Subscription, User, Link, Vote}
 
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8'),
@@ -18,6 +21,7 @@ const server = new ApolloServer({
   context: ({req}) => ({
     ...req,
     prisma,
+    pubsub,
     userId: req && req.headers.authorization ? getUserId(req) : null,
   }),
 })
